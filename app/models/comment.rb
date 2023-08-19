6cb9180318579c0c0ebb3b7a36164f6179f5e5fc
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-08-19 07:15:06 +0300

    Create Comment associations, and custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,20 @@
 class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  after_create :update_comments_counter_on_create
+  after_destroy :update_comments_counter_on_destroy
+
+  def update_comments_counter_on_create
+    update_comments_counter(1)
+  end
+
+  def update_comments_counter_on_destroy
+    update_comments_counter(-1)
+  end
+
+  def update_comments_counter(value)
+    post.comments_counter += value
+    post.save
+  end
 end
