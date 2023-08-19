d64361871595a0945cb686943ba8f2c967a541ad
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-08-19 09:43:44 +0300

    Add unit tests for all models and their methods

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save comment without user' do
+    comment = Comment.new
+    assert_not comment.save
+  end
+
+  test 'should not save comment without post' do
+    comment = Comment.new
+    assert_not comment.save
+  end
+
+  test 'should save comment with user and post' do
+    comment = Comment.new
+    comment.author = User.new
+    comment.post = Post.new
+    assert comment.save
+  end
+
+  test 'should not save comment with user and without post' do
+    comment = Comment.new
+    comment.author = User.new
+    assert_not comment.save
+  end
+
+  test 'should not save comment without user and with post' do
+    comment = Comment.new
+    comment.post = Post.new
+    assert_not comment.save
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save post without title' do
+    post = Post.new
+    assert_not post.save
+  end
+
+  test 'should not save post without author' do
+    post = Post.new
+    assert_not post.save
+  end
+
+  test 'should not save post with title and without author' do
+    post = Post.new
+    post.title = 'Hello'
+    assert_not post.save
+  end
+
+  test 'should not save post without title and with author' do
+    post = Post.new
+    user = User.new(name: 'John')
+    post.author = user
+    assert_not post.save
+  end
+
+  test 'should save post with title and author' do
+    post = Post.new
+    post.title = 'Hello'
+    user = User.new(name: 'John')
+    post.author = user
+    assert post.save
+  end
+
+  test 'should fetch five most recent comments' do
+    user = User.create(name: 'John')
+
+    post = Post.create(title: 'Hello', author: user)
+
+    _comment1 = Comment.create(author: user, post:, text: 'Hi')
+    comment2 = Comment.create(author: user, post:, text: 'Hi')
+    comment3 = Comment.create(author: user, post:, text: 'Hi')
+    comment4 = Comment.create(author: user, post:, text: 'Hi')
+    comment5 = Comment.create(author: user, post:, text: 'Hi')
+    comment6 = Comment.create(author: user, post:, text: 'Hi')
+
+    assert_equal post.five_most_recent_comments, [comment6, comment5, comment4, comment3, comment2]
+  end
 end
