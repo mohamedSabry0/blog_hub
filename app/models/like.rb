516efa239e711718355af81fdcbd4f92976944b1
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-08-19 07:15:35 +0300

    Create Like model associations, and custom methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,20 @@
 class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  after_create :update_likes_counter_on_create
+  after_destroy :update_likes_counter_on_destroy
+
+  def update_likes_counter_on_create
+    update_likes_counter(1)
+  end
+
+  def update_likes_counter_on_destroy
+    update_likes_counter(-1)
+  end
+
+  def update_likes_counter(value)
+    post.likes_counter += value
+    post.save
+  end
 end
