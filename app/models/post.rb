136b147c87bf1658f0b8a38f6af4727101b59cc9
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-08-19 07:14:32 +0300

    Create Post model associations, and custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments, dependent: :destroy
+
+  after_create :update_likes_counter_on_create
+  after_destroy :update_likes_counter_on_destroy
+
+  def update_likes_counter_on_create
+    update_likes_counter(1)
+  end
+
+  def update_likes_counter_on_destroy
+    update_likes_counter(-1)
+  end
+
+  def update_likes_counter(value)
+    author.likes_counter += value
+    author.save
+  end
+
+  def five_most_recent_own_comments
+    Comment.where(post: self).order(created_at: :desc).limit(5)
+  end
 end
