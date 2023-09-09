c6b1fec33dd39ef9c622c8554d44b270271ee187
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-09-10 01:11:58 +0300

    Enable user to login, change password

---
+++ app/controllers/users/confirmations_controller.rb
@@ -0,0 +1,36 @@
+class Users::ConfirmationsController < Devise::ConfirmationsController
+  # GET /resource/confirmation/new
+  # def new
+  #   super
+  # end
+
+  # POST /resource/confirmation
+  def create
+    user = User.find_by(email: params[:user][:email])
+
+    if user && !user.confirmed?
+      user.confirm
+      sign_in(user)
+      redirect_to users_path
+    else
+      redirect_to new_user_confirmation_path
+    end
+  end
+
+  # GET /resource/confirmation?confirmation_token=abcdef
+  # def show
+  #   super
+  # end
+
+  # protected
+
+  # The path used after resending confirmation instructions.
+  # def after_resending_confirmation_instructions_path_for(resource_name)
+  #   super(resource_name)
+  # end
+
+  # The path used after confirmation.
+  # def after_confirmation_path_for(resource_name, resource)
+  #   super(resource_name, resource)
+  # end
+end
